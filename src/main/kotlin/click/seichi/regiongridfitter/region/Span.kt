30a4879afcd430ae17a2188f4f5e9dd674952330
Kory33 <6561358+kory33@users.noreply.github.com>
2018-03-23 22:50:06 +0800

    fixes calculation of span quantization

--- src/main/kotlin/click/seichi/regiongridfitter/region/Span.kt
+++ src/main/kotlin/click/seichi/regiongridfitter/region/Span.kt
@@ -1,6 +1,5 @@
 package click.seichi.regiongridfitter.region
 
-import kotlin.math.ceil
 import kotlin.math.floor
 
 /**
@@ -15,10 +14,14 @@ typealias RealSpan = Span<Double>
 
 /**
  * レシーバの区間を含むかつ[unitSize]で量子化された最小の区間を計算する。
+ * 量子化された区間は、とある整数k, a, b(a < b)に対し、`[ka, kb - 1]`となるものである。
+ * とある[Double] aを中心とする空の区間`[a, a]`は、aが属する量子化された区間に変換される。
  */
 fun RealSpan.getQuantizedEnclosure(unitSize: Int): RealSpan {
-    val newLargeEnd = ceil(largeEnd / unitSize.toDouble()) * unitSize
-    val newSmallEnd = floor(smallEnd / unitSize.toDouble()) * unitSize
+    fun quantizedBlockNumber(position: Double) = floor(position / unitSize.toDouble())
+
+    val newLargeEnd = (quantizedBlockNumber(largeEnd) + 1) * unitSize - 1.0
+    val newSmallEnd = quantizedBlockNumber(smallEnd) * unitSize
 
     return RealSpan(newSmallEnd, newLargeEnd)
 }
